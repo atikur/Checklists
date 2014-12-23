d78a7063338c505aa51b5d27c77d78d85a665c96
Atikur Rahman <atik.bd08@gmail.com>
2014-12-23 16:19:19 +0600

    tableview with dummy checklist items

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -9,17 +9,78 @@
 import UIKit
 
 class ChecklistViewController: UITableViewController {
+    
+    var checklistItems: [ChecklistItem] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        addChecklistItems()
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return checklistItems.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("ChecklistItem") as UITableViewCell
+        let item = checklistItems[indexPath.row]
+
+        configureTextLabelForCell(cell, withChecklistItem: item)
+        configureCheckmarkForCell(cell, withChecklistItem: item)
+        
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        if let cell = tableView.cellForRowAtIndexPath(indexPath) {
+            let item = checklistItems[indexPath.row]
+            item.toggleChecked()
+            
+            configureCheckmarkForCell(cell, withChecklistItem: item)
+        }
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // helper methods
+    
+    private func configureCheckmarkForCell(cell: UITableViewCell, withChecklistItem item: ChecklistItem) {
+        if item.checked {
+            cell.accessoryType = UITableViewCellAccessoryType.Checkmark
+        } else {
+            cell.accessoryType = UITableViewCellAccessoryType.None
+        }
+    }
+    
+    private func configureTextLabelForCell(cell: UITableViewCell, withChecklistItem item: ChecklistItem) {
+        let label = cell.viewWithTag(1000) as UILabel
+        label.text = item.text
+    }
+    
+    private func addChecklistItems() {
+        let item1 = ChecklistItem()
+        item1.text = "Wake up"
+        checklistItems.append(item1)
+        
+        let item2 = ChecklistItem()
+        item2.text = "Have breakfast"
+        checklistItems.append(item2)
+        
+        let item3 = ChecklistItem()
+        item3.text = "Learn iOS development"
+        checklistItems.append(item3)
+        
+        let item4 = ChecklistItem()
+        item4.text = "Read newspaper"
+        checklistItems.append(item4)
+        
+        let item5 = ChecklistItem()
+        item5.text = "Have a walk"
+        checklistItems.append(item5)
+    }
 }
 
