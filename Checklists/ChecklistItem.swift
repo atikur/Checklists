fa47f4941db0fccc9e8b54959f9f6becf1b502a7
Atikur Rahman <atik.bd08@gmail.com>
2014-12-23 15:10:55 +0600

    add ChecklistItem class

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,14 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Atikur Rahman on 12/23/14.
+//  Copyright (c) 2014 Atikur Rahman. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+}
