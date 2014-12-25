2f84fb96d9403a8a72d8198d39e9b726c8bf6de0
Atikur Rahman <atik.bd08@gmail.com>
2014-12-25 15:57:03 +0600

    add Checklist class

---
+++ Checklists/Checklist.swift
@@ -0,0 +1,13 @@
+//
+//  Checklist.swift
+//  Checklists
+//
+//  Created by Atikur Rahman on 12/25/14.
+//  Copyright (c) 2014 Atikur Rahman. All rights reserved.
+//
+
+import UIKit
+
+class Checklist: NSObject{
+    var name: String = ""
+}
